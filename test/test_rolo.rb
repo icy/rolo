8b7d0170bc161cc39baa48b4eb1721bc654f5efc
Ky-Anh Huynh <kyanh@theslinux.org>
2017-08-27 12:48:14 +0700

    tests: use random port. fix redirection issue
    We can not use redirection 2>&1 in exec command. Tests fail randomly because of that

--- test/test_rolo.rb
+++ test/test_rolo.rb
@@ -5,7 +5,7 @@ require 'test/unit'
 
 class RoloTest < Test::Unit::TestCase
   @@rolo = File.join(File.dirname(__FILE__), "../bin/rolo")
-  @@port = ENV['ROLO_TEST_PORT'] || '10002'
+  @@port = ENV['ROLO_TEST_PORT'] || (rand(65000 - 1024) + 1024)
   @@addr = ENV['ROLO_TEST_ADDR'] || '127.0.0.1'
 
   def test_low_port
@@ -19,10 +19,20 @@ class RoloTest < Test::Unit::TestCase
   end
 
   def test_running
-    (pid = fork) ? Process.detach(pid) : exec("#{@@rolo} --address #{@@addr} --port #{@@port} sleep 3 2>&1")
-    ret = %x{#{@@rolo} --port #{@@port} --address #{@@addr} sleep 2 2>&1}
+    num_secs = 3
+
+    test_cmd = "#{@@rolo} --address #{@@addr} --port #{@@port} sleep #{num_secs}"
+
+    (pid = Process.fork) ? Process.detach(pid) : exec(test_cmd)
+
+    ret = %x[#{test_cmd} 2>&1]
     assert_match /your application running/, ret
-    begin Process.wait(pid) rescue nil ; end
-    sleep(3)
+
+    begin
+      STDERR.puts ":: Wait #{num_secs} seconds for cleaning up..."
+      Process.wait(pid)
+    rescue
+      nil
+    end
   end
 end
